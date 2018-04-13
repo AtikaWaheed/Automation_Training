d7193e66548e4fe46b35cfc0c77fccbaa695a5fa
AtikaWaheed <atikawaheed@hotmail.com>
2018-04-13 15:06:07 +0500

    Some changings

--- NewAssignment/tests/test_automation_form.py
+++ NewAssignment/tests/test_automation_form.py
@@ -48,7 +48,7 @@ dict = {
                               'correct_input': 'upload_files'},
         'On_a_scale_of_1_to_five_how_hard_this_assignment_is': {'func_to_call_for_empty_field': 'error_with_empty',
                                                                 'empty_error': 'This is a required question',
-                                                                'correct_input': 'scale_numbr'},
+                                                                'correct_input': 'scale_ranking'},
         'How_satisfied_are_you_with_the_following': {'func_to_call_for_empty_field': 'error_with_empty',
                                                      'empty_error': 'This question requires one response per row',
                                                      'correct_input': 'choice_grid'},
@@ -84,7 +84,6 @@ class TestGoogleForm(unittest.TestCase):
         query2 = os.environ.get("password")
         try:
             cls.google_page.sign_in_button()
-
         except NoSuchElementException:
             cls.google_page.visit()
             cls.driver.implicitly_wait(10)
@@ -123,36 +122,37 @@ class TestGoogleForm(unittest.TestCase):
         total_score_get = self.google_page.total_score()
         total_score_get.split()
         print "Got " + total_score_get[0] + total_score_get[1] + " points out of 40"
+        """
+        writing correct answers in output csv
+        """
         my_file = open("output.csv", "w")
         for number in range(1, 4):
             for nmb in range(2):
                 title = self.google_page.grading_question_list(number, nmb)
-                answr_list = self.google_page.correct_input(number, nmb)
+                answr_list = self.google_page.correct_answers(number, nmb)
                 if len(answr_list) != 0:
                     my_file.write('Q: ' + str(title) + "\n")
                     for item in answr_list:
                         my_file.write(str(item.text) + "\n")
         self.google_page.edit_response()
         self.google_page.click_next_button()
-
+        """
+        Read each question from csv and put correct input 
+        """
         my_file = 'output.csv'
         with open(my_file, 'r') as f:
             csv_reader = csv.reader(f)
             for row in csv_reader:
-                print 'ROW' + str(row)
                 if row[0].startswith('Q:'):
                     question = row[0].replace('Q: ', '')
-                    print 'Question' + str(question)
                     question_found = False
                     for number in range(2):
                         all_headings = self.google_page.questions_headings()
                         for ind, title in enumerate(all_headings):
-                            print 'ind' + str(ind)
                             if title.text == question:
                                 next_answer = next(csv_reader)
-                                print 'next_answer' + next_answer[0]
                                 self.google_page.first_uncheck_all_options(ind)
-                                self.google_page.correct_inputs(ind, next_answer[0])
+                                self.google_page.correct_input_from_csv(ind, next_answer[0])
                                 question_found = True
                                 break
                         if question_found:
@@ -160,12 +160,12 @@ class TestGoogleForm(unittest.TestCase):
                         else:
                             self.google_page.click_next_button()
                 else:
-                    self.google_page.multiple_inputs(row[0])
+                    self.google_page.multiple_inputs_from_csv(row[0])
         """
-        Click next to until you reach last page to resubmit form
+        Click next until you reach last page to resubmit form
         """
         [self.google_page.click_next_button() for _ in range(7)]
-        self.google_page.view_submit_form()
+        self.google_page.view_submitted_form()
         if self.google_page.total_score() == "40/40":
             print "Finally you did it"
 
